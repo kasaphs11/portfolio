e651529658a0bf8eeb935bcbd42df1cd13581d95
kasaphs11 <kas11paok@gmail.com>
2026-04-16 14:54:18 +0300

    new

--- src/script.js
+++ src/script.js
@@ -127,7 +127,7 @@ const getLayoutConfig = () =>
         return {
             cameraZ: 8.35,
             shapeX: 0,
-            shapeY: 1,
+            shapeY: 0,
             shapeScale: 0.74,
             floatAmount: 0.045
         }
